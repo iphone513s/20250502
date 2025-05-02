1a5bb44643ff7b574b44742a7e6ea4f0fd1cb10d
iphone513s <43226563+iphone513s@users.noreply.github.com>
2025-05-02 10:26:15 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,8 +17,14 @@ function draw() {
   let x = (width - capture.width) / 2;
   let y = (height - capture.height) / 2;
   
+  // 翻轉畫布以左右顛倒影像
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  
   // 繪製攝影機影像
   image(capture, x, y, capture.width, capture.height);
+  pop();
 }
 
 function windowResized() {
